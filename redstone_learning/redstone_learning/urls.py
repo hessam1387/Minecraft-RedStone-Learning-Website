9d892b83db5f6022e78821b2a4635e315145484f
hessam1387 <hasangoodarzi13877@gmail.com>
2026-04-30 16:47:15 +0330

    sosis

--- redstone_learning/redstone_learning/urls.py
+++ redstone_learning/redstone_learning/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include("main.urls")),
-    path('newyear', include("newyear.urls"))
+    path('newyear/', include("newyear.urls")),
+    path('taskmanager/', include("taskmanager.urls"))
 ]
---
+++ redstone_learning/taskmanager/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+tasks = ['abbas', 'mmd', 'akbar']
+
+def index(request):
+
+    return render(request, 'taskmanager/index.html', {
+        "tasks" : tasks
+    })
+
+
+def add(request):
+
+    return render(request, 'taskmanager/add.html', {
+        
+})
